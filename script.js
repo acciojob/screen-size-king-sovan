5c5fb3a9f460a0e4e02948f717ee0d0d0e66a56b
king-sovan <156076876+king-sovan@users.noreply.github.com>
2024-10-08 13:38:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function updateWindowSize() {
+	const width = window.innetWidth;
+	const height = window.innerHeight;
+	document.getElementById('windowSize').textContent = `Width: ${width} and Height: ${height}`;
+}
+
+updateWindowSize();
+
+window.addEventListener('resize', updateWindowSize);
